2ea10e910ccbe9be9ecd7660ce24bb07c32235e6
Pratik <pratikpatil8560@gmail.com>
2023-02-14 16:14:29 +0530

    Integreted API with FrontEnd

--- index.js
+++ index.js
@@ -58,8 +58,13 @@ const productSchema = {
 
 const Product = mongoose.model("Product", productSchema);
 
+
+
+
+
+
 app.get("/", (req, res) => {
-  res.send("allproducts");
+  res.render("home");
 });
 
 app.get("/viewall", (req, res) => {
@@ -72,30 +77,34 @@ app.get("/viewall", (req, res) => {
 app.get("/featured", (req, res) => {
   Product.find({ isFeatured: true }, (err, data) => {
     if (err) console.log(err);
-    else res.send(data);
+    else res.render("viewall", { products: data });
   });
 });
 
 app.get("/create", (req, res) => {
-  res.render("create");
+  res.render("create-update", {
+    title: "Add product",
+    route: "create",
+    prodID: "",
+    required: "required",
+    desc: "This is required field.",
+    msg: "",
+    readonly: "",
+  });
 });
 
-app.get("/update", (req, res) => {
-  res.render("update");
+app.get("/filter", (req, res) => {
+  res.render("filter");
 });
 
-app.get("/failure", (req, res) => {
-  res.render("failure");
-});
-app.get("/success", (req, res) => {
-  res.render("success");
-});
+
+
+
+
+
+
 
 app.post("/create", (req, res) => {
-  // if(req.body.prodIsFeatured === 'true')
-  // var featureStatus = true;
-  // else featureStatus = false;
-
   const newProduct = new Product({
     productID: req.body.prodID,
     name: req.body.prodName,
@@ -111,46 +120,23 @@ app.post("/create", (req, res) => {
       console.log(err);
     }
   });
-  res.redirect("/");
+  res.render("success");
 });
-
-// app.post("/update", (req, res)=> {
-//   const change = {
-//     name: req.body.prodName,
-//     price: req.body.prodPrice,
-//     isFeatured: req.body.prodIsFeatured,
-//     rating: req.body.prodRating,
-//     createdAt: req.body.prodDate,
-//     company: req.body.prodCompany,
-//   };
-//   Product.findOneAndUpdate(
-//     { productID: req.body.prodID },
-//     change,
-//     (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.redirect("/failure");
-//       } else {
-//         res.redirect('/success');
-//         console.log("Data updated!");
-//       }
-//     }
-//   );
-//});
 
 app.post("/update", (req, res) => {
   Product.findOne({ productID: req.body.prodID }, (err, product) => {
+    console.log(req.body.prodID);
     if (err) {
       console.log(err);
-      res.redirect("/failure");
+      res.render("failure");
     } else if (!product) {
       console.log("Product not found");
-      res.redirect("/failure");
+      res.render("failure");
     } else {
       const change = {
         name: req.body.prodName,
         price: req.body.prodPrice,
-        isFeatured: req.body.prodIsFeatured,
+        isFeatured: req.body.prodIsFeatured === "true" ? true : false,
         rating: req.body.prodRating,
         createdAt: req.body.prodDate,
         company: req.body.prodCompany,
@@ -161,9 +147,9 @@ app.post("/update", (req, res) => {
         (err, data) => {
           if (err) {
             console.log(err);
-            res.redirect("/failure");
+            res.render("failure");
           } else {
-            res.redirect("/success");
+            res.render("success");
             console.log("Data updated!");
           }
         }
@@ -173,36 +159,60 @@ app.post("/update", (req, res) => {
 });
 
 app.post("/delete", (req, res) => {
-  Product.deleteOne({ productID: req.body.prodID }, function (err, data) {
+  Product.deleteOne({ _id: req.body._ID }, function (err, data) {
     if (err) {
       console.log(err);
+      res.redirect("/failure");
     } else {
-      res.send(data);
+      res.redirect("/viewall");
     }
   });
 });
 
-app.post("/plt", (req, res) => {
-  //price less than
-  Product.find({ price: { $lte: req.body.prodPrice } }, (err, data) => {
-    if (err) console.log(err);
-    else res.send(data);
+app.post("/plt", async (req, res) => {
+  //to show products with price less than given price
+  const price = req.body.prodPrice;
+
+  try {
+    const products = await Product.find({ price: { $lt: price } });
+    res.render("viewall", { products: products });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.post("/rgt", async (req, res) => {
+  //to show products with price less than given price
+  const rating = req.body.prodRating;
+
+  try {
+    const products = await Product.find({ rating: { $gt: rating } });
+    res.render("viewall", { products: products });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.post("/updt", (req, res) => {
+  const ID = req.body.prodID;
+  console.log(ID);
+  res.render("create-update", {
+    title: "Update product details",
+    route: "update",
+    prodID: ID,
+    required: "",
+    desc: "Check the product ID.",
+    msg: "Add full information of the product.",
+    readonly: "readonly",
   });
 });
 
-app.post("/rgt", (req, res) => {
-  //rating greater than
-  Product.find({ rating: { $gte: req.body.prodRating } }, (err, data) => {
-    if (err) console.log(err);
-    else res.send(data);
-  });
-});
 
-app.post("/viewall", (req, res) => {
-  res.redirect("/viewall");
-});
 
-const PORT = 4000 || process.env.PORT;
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("server started on port " + PORT);
 });
